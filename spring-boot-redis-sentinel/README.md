def5ad528e162a10ba722bcccfcd4fccd00400f1
Jason.Wang <kzone.wang@gmail.com>
2020-03-16 14:52:53 +0800

    add spring boot redis data with redis sentinel

---
+++ spring-boot-redis-sentinel/README.md
@@ -0,0 +1,5 @@
+## Spring Boot Redis Sentinel
+
+Spring Boot With Redis Sentinel
+
+Spring Boot 使用 Redis Sentinel 集群
---
+++ spring-boot-redis-sentinel/src/main/java/me/kany/project/learning/spring/entity/BaseResponse.java
@@ -0,0 +1,41 @@
+/**
+ * Project Name:learning-spring
+ * File Name:BaseResponse.java
+ * Package Name:me.kany.project.learning.spring.entity
+ * Date:2020年03月16日 10:42
+ * Copyright (c) 2020, Jason.Wang All Rights Reserved.
+ */
+package me.kany.project.learning.spring.entity;
+
+import lombok.Data;
+
+/**
+ * ClassName:BaseResponse<br/>
+ * Function: 默认返回类型<br/>
+ * Date:2020年03月16日 10:42<br/>
+ *
+ * @author Jason.Wang
+ * @version 1.0.0.0
+ * @see
+ * @since JDK1.8
+ */
+@Data
+public class BaseResponse {
+    private int code;
+    private String message;
+    private Object data;
+
+    public BaseResponse() {
+    }
+
+    public BaseResponse(RetCode retcode) {
+        this.code = retcode.getCode();
+        this.message = retcode.getMessage();
+    }
+
+    public BaseResponse(RetCode retcode, Object data) {
+        this.code = retcode.getCode();
+        this.message = retcode.getMessage();
+        this.data = data;
+    }
+}
